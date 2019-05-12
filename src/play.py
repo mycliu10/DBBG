92471cbc6052cc06f5d2cb1fb4fc089386962de6
yuchen <yl8bc@mst.edu>
2019-05-12 18:45:39 +0000

    Added Player class

--- src/play.py
+++ src/play.py
@@ -1,8 +1,40 @@
 import gnubg as bg
 import numpy as np
 
+class Player:
+    def __init__(self):
+        return
 
-def PlayGame(board, num_games=8):
+    def reset(self):
+        bg.command("new match")
+        bg.command("set automatic game on") # Auto play
+        bg.command("set jacoby off") # I don't know, I just turned it off anyway...
+        bg.command("set crawford off") # I don't know, I just turned it off anyway...
+        bg.command("set automatic crawford off")
+        bg.command("set automatic roll off")
+        bg.command("set automatic move off")
+        bg.command("set matchlength 0")
+
+        bg.command("set cube use on")
+        bg.command("set player 0 name O")
+        bg.command("set player 1 name X")
+        bg.command("new game")
+        self._move_count = 0
+        self._cube_owner = bg.cubeinfo()['cubeowner']
+
+    def step(self, action):
+        cube_info = bg.cubeinfo()
+        cube_owner = cube_info['cubeowner']
+        bg.command("swap players")
+        bg.command("play")
+        bg.command("swap players")
+        bg.command("play")
+        game_info = bg.match(0,1,0,0)['games'][0]['info']
+        winner = game_info['winner']
+        print(game_info)
+#
+
+def PlayGame(board, num_games=1):
     bg.command("set automatic game on") # Auto play
     bg.command("set jacoby off") # I don't know, I just turned it off anyway...
     bg.command("set crawford off") # I don't know, I just turned it off anyway...
@@ -19,14 +51,27 @@ def PlayGame(board, num_games=8):
     command_set_board = "set board simple " + ' '.join(str(x) for x in board)
     for count in range(num_games):
         bg.command("new game")
-        bg.command(command_set_board)
-        print("BOARD IS SET!!!")        
-        board = bg.board()
+#        bg.command(command_set_board)
+#        print("BOARD IS SET!!!")        
+#        board = bg.board()
 #        simple_board = BoardPositionToSimple(board)
 #        print(board)
 #        print(simple_board)
-
-        bg.command("end game")
+        
+        winner = None
+        while winner is None:
+            cube_info = bg.cubeinfo()
+            cube_owner = cube_info['cubeowner']
+#            whosmove = cube_info['move']
+            bg.command("swap players")
+            bg.command("play")
+            bg.command("swap players")
+            bg.command("play")
+            game_info = bg.match(0,1,0,0)['games'][0]['info']
+            winner = game_info['winner']
+            print(game_info)
+#            raw_input("Press Enter to continue...")
+#        bg.command("end game")
 
 #    bg.command("end match")
     match_info = bg.match(0,1,0,0)['games']
@@ -38,9 +83,9 @@ def PlayGame(board, num_games=8):
         if winner is None:
             points_won = 0
         elif winner=='X':
-            points_won = game_info['info']['points-won']
+            points_won = -game_info['info']['points-won']
         elif winner=='O':
-            points_won = - game_info['info']['points-won']
+            points_won = game_info['info']['points-won']
         else:
             raise KeyError("NO INFO!")
         print(winner, points_won)
@@ -57,4 +102,4 @@ def BoardPositionToSimple(board):
     return tuple(simple)
 
 
-PlayGame((1, 0, 0, 1, 2, 0, 5, 0, 2, 0, -1, 0, -3, 3, 0, 0, 0, -2, -2, -5, 0, 1, -2, 0, 0, 0), num_games=9)
+PlayGame((1, 0, 0, 1, 2, 0, 5, 0, 2, 0, -1, 0, -3, 3, 0, 0, 0, -2, -2, -5, 0, 1, -2, 0, 0, 0), num_games=1)
